a048dc27248d3cc0a6dc13402d19e57b78c4fc71
klypsocialofficial-maker <klypsocialofficial@gmail.com>
2026-06-03 09:41:58 -0300

    s

--- src/sw.ts
+++ src/sw.ts
@@ -77,18 +77,13 @@ clientsClaim();
 (self as any).importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js');
 (self as any).importScripts('https://www.gstatic.com/firebasejs/10.11.0/firebase-messaging-compat.js');
 
+import firebaseConfig from '../firebase-applet-config.json';
+
 // @ts-ignore
 const firebase = (self as any).firebase;
 
 if (firebase) {
-  firebase.initializeApp({
-    apiKey: "AIzaSyBw-s1k1pYD_P4NwUDQtf3-7vVesgzNGtU",
-    authDomain: "gen-lang-client-0405663991.firebaseapp.com",
-    projectId: "gen-lang-client-0405663991",
-    storageBucket: "gen-lang-client-0405663991.firebasestorage.app",
-    messagingSenderId: "948218731980",
-    appId: "1:948218731980:web:b9f949d01ea2082c631169"
-  });
+  firebase.initializeApp(firebaseConfig);
 
   const messaging = firebase.messaging();
 
